9bc27804f438fa611568c7d00be2e4f176d543e6
jab_yum <jab_yum@github.com>
2025-04-29 18:32:17 +0500

    second

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from telebot import TeleBot
 from buttons import *
+import database as db
 from geopy import Photon
 bot = TeleBot("7671651212:AAFLNOKURS6o7mQBNrvbt49Jgmqo82MaIqo")
 # объект работающий с локацией
@@ -7,9 +8,13 @@ geolocator = Photon(user_agent="geo-locator", timeout=10)
 @bot.message_handler(commands=["start"])
 def start(message):
     user_id = message.from_user.id
-    bot.send_message(user_id, "Добро пожаловать в бот доставки!\n"
-                              "Пожалуйста, введите свое имя")
-    bot.register_next_step_handler(message, get_name)
+    checker = db.check_user(user_id)
+    if checker == True:
+        bot.send_message(user_id, "Главное меню:")
+    elif checker == False:
+        bot.send_message(user_id, "Добро пожаловать в бот доставки!\n"
+                                  "Пожалуйста, введите свое имя")
+        bot.register_next_step_handler(message, get_name)
 def get_name(message):
     user_id = message.from_user.id
     name = message.text
@@ -22,10 +27,11 @@ def get_phone(message, name):
     user_id = message.from_user.id
     if message.contact:
         phone_number = message.contact.phone_number
+        db.add_user(user_id, name, phone_number)
         bot.send_message(user_id, f"Вы успешно прошли регистрацию, {name}!\n"
                                   f"Ваш номер: {phone_number}")
-        bot.send_message(user_id, "Отправьте свою локацию", reply_markup=location_bt())
-        bot.register_next_step_handler(message, get_location)
+        # bot.send_message(user_id, "Отправьте свою локацию", reply_markup=location_bt())
+        # bot.register_next_step_handler(message, get_location)
     else:
         bot.send_message(user_id, "Отправьте свой номер через кнопку в меню.",
                          reply_markup=phone_number_bt())
